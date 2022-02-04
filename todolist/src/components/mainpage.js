a49c63297ad823d052832b79b039fb9c8f5bad9c
Akshaj000 <akshajsr000@gmail.com>
2022-02-05 01:35:19 +0530

    Edited cards

--- todolist/src/components/mainpage.js
+++ todolist/src/components/mainpage.js
@@ -9,45 +9,47 @@ export default function Page(){
     }
     const cardstyle ={
         margin:"13px",
+        padding:"6px",
         "WebkitFlex": "1",
         "msFlex": "1",
         flex: "1" 
     }
 
     return(
-      <div className="card" style={cardstyle}>
-        <div className="container" style={mystyle}>
+      <div className="card-body" style={cardstyle}>
             <div className="card-deck">
                 <Card title="Buy chicken" description="aka kill the poor little innocent chicken:\" />
                 <Card/>
                 <Card/>
                 <Card/>
             </div>
-        </div>
     </div>
     );
 }
 
 function Card(props){
     const buttonstyle ={
-        margin:"1px",
+        margin:"3px",
     }
     const cardstyle={
         "WebkitFlex": "1",
-        margin : "5px"
+        margin : "5px",
+        "max-width": "18rem"
     }
 
     return(
         <>
-        <div className="card mb-5 box-shadow" style={cardstyle}>
-            <div className="card" >
+        <div className="card text-white bg-dark mb-3" style={cardstyle}>
+            <div className="card-header" >
                 <h5 className="card-title">{props.title}</h5>
-                <p className="card-text">{props.description}</p>
             </div>
-            <div className="card-footer">
-                <Button className="btn btn-sm btn-outline-success" name="DONE" style={buttonstyle} />
-                <Button className="btn btn-sm btn-outline-warning" name="EDIT" style={buttonstyle}/>
-                <Button className="btn btn-sm btn-outline-danger"  name="DELETE" style={buttonstyle}/>
+            <div className="card-body">
+            <p className="card-text">{props.description}</p>
+            </div>
+            <div className="card-footer row">
+                <Button className="btn btn-sm btn-success" name="DONE" style={buttonstyle} />
+                <Button className="btn btn-sm btn-warning" name="EDIT" style={buttonstyle}/>
+                <Button className="btn btn-sm btn-danger"  name="DELETE" style={buttonstyle}/>
             </div>
 
         </div>
