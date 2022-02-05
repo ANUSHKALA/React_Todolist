d0d623dbc07e8d38951496769cb7a940831fa012
Akshaj000 <akshajsr000@gmail.com>
2022-02-05 14:04:26 +0530

    Added a json file to map tasks

--- todolist/src/jsondata/cards.js
+++ todolist/src/jsondata/cards.js
@@ -1,54 +1,38 @@
 export var Carddata = [
-
     {
-
         name: "Twitter Inc",
-
         description: "sddsc sc chickednw cwqnvceuiwv TWTR",
-
+        category:"Shopping list"
     },
 
     {
-
         name: "Square Inc",
-
         description: "sdvsrfve rgvrsddsc sc chickednw cwqnvceuiwv SQ",
-
-
-
+        category:"Shopping list",
     },
 
     {
-
         name: "Shopify Inc",
-
         description: "trtretgt ger vgrsddsc sc chickednwrfewrg cwqnvceuiwv SHOP",
-
-
-
+        category:"Shopping list",
     },
 
     {
-
         name: "Sunrun Inc",
-
         description: "w4ft reqgetge sddsc sc chickednw cwqnvceuiwv RUN",
-
+        category:"Shopping list",
     },
 
     {
-
         name: "Adobe Inc",
-
         description: "xxx w fqrfgqrgv sddsc sc chickednw cwqnvceuiwv ADBE",
-
+        category:"Shopping list",
     },
 
     {
-
         name: "HubSpot Inc",
-
         description: "zxzxc wfvrg sddsc sc chickednw cwqnvceuiwv HUBS",
+        category:"Shopping list",
     },
 
 ];
